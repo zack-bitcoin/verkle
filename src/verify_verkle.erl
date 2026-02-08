784fa76fc2b3a6dfc22c065bdefa616afa2d9453
zack <zack.bitcoin@gmail.com>
2026-02-08 16:52:02 -0300

    print statement to diagnose error

--- src/verify_verkle.erl
+++ src/verify_verkle.erl
@@ -442,6 +442,7 @@ proof({Tree0, CommitG0, Open0}) ->
             %io:fwrite("verify multiproof \n"),
             benchmark:now(),
             %io:fwrite({Zs, Ys}),
+	    io:fwrite({{CommitG, Open}, Commits, Zs, Ys}),
             B2 = multiproof:verify(
                    {CommitG, Open}, 
                    Commits, Zs, Ys),
