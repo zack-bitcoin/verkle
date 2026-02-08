41a0e3ea78f662078b05cfa6549a37ed7da8bdb7
zack <zack.bitcoin@gmail.com>
2026-02-08 17:08:35 -0300

    remove unused print statment

--- src/verify_verkle.erl
+++ src/verify_verkle.erl
@@ -449,7 +449,6 @@ proof({Tree0, CommitG0, Open0}, CFG) ->
             %io:fwrite("verify multiproof \n"),
             benchmark:now(),
             %io:fwrite({Zs, Ys}),
-	    io:fwrite({{CommitG, Open}, Commits, Zs, Ys}),
             B2 = multiproof:verify(
                    {CommitG, Open}, 
                    Commits, Zs, Ys),
